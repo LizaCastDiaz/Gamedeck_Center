51f2eb323eb2bc4f78e3de09c7db73a49e094e74
LizaCastDiaz <lizacastdiaz@gmail.com>
2022-12-14 16:48:28 +0100

    Adding the register, login, logout endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,8 +10,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//POST
 Route::post('/register', [AuthController::class, 'register']);
+//POST
 Route::post('/login', [AuthController::class, 'login']);
+
 Route::group([
 'middleware' => 'jwt.auth'
 ], function () {
