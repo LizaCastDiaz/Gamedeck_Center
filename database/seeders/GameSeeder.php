70c048454b0c44cdf1fceae98f29a8f2dc686c84
LizaCastDiaz <lizacastdiaz@gmail.com>
2022-12-14 22:42:58 +0100

    Migrations, models for users, games and parties are created

---
+++ database/seeders/GameSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GameSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        
+        // I does guard your model attributes. Only these you define in $fillable will be updated in database,
+    }
+}
